a2c665ab0005ee82a77e0e31368df4db3300ba2a
Taranjeet Singh <reachtotj@gmail.com>
2019-05-25 14:00:55 +0530

    add list view of books.

--- src/cbvlibrary/urls.py
+++ src/cbvlibrary/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
 ]
